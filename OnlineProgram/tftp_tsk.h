17b7016bcd43409ee94dda46953320df5d933b2f
Sadohara <zz033130@oki.com>
2020-11-10 12:04:17 +0900

    [DEV-tftp] DEBUG時に内部関数や変数を外部参照可能なようにした

--- OnlineProgram/tftp_tsk.h
+++ OnlineProgram/tftp_tsk.h
@@ -11,6 +11,45 @@
 extern void downld_thread( void *arg );
 extern void writer_thread( void *arg );
 
+#ifdef DEBUG
+/* 状態番号 */
+extern TFTP_STATE_e		downld_state_no;
+/* タイマ登録ID(1～254) */
+extern pBYTE				downld_tim;
+/* ダウンロード完了判断カウンタ */
+extern uint16_t			downld_same_cnt;
+/* TFTP起動前のLED状態 */
+extern uint8_t			prev_LED;
+/* errno文字列生成バッファ */
+extern char		str_buff[16];
+/* ダウンロード種別 */
+extern uint16_t			downld_type;
+/* 内部関数 */
+extern void downld_init(void);
+extern void downld_SttMng(INNER_MSG *msg_p);
+extern BYTE dl_IdleStt_SvrReq( INNER_MSG *msg_p );
+extern BYTE dl_IdleStt_Cl1Req( INNER_MSG *msg_p );
+extern BYTE dl_IdleStt_Cl2Req( INNER_MSG *msg_p );
+extern BYTE dl_SvrStt_SvrReq( INNER_MSG *msg_p );
+extern BYTE dl_SvrStt_Tmo( INNER_MSG *msg_p );
+extern BYTE dl_SvrStt_Cl1Req( INNER_MSG *msg_p );
+extern BYTE dl_SvrStt_Cl2Req( INNER_MSG *msg_p );
+extern BYTE dl_SvrStt_WriteResp( INNER_MSG *msg_p );
+extern BYTE dl_ClStt_SvrReq( INNER_MSG *msg_p );
+extern BYTE dl_ClStt_Cl1Req( INNER_MSG *msg_p );
+extern BYTE dl_ClStt_Cl2Req( INNER_MSG *msg_p );
+extern BYTE dl_ClStt_WriteResp( INNER_MSG *msg_p );
+extern bool dl_tmpfile_chk(void);
+extern BYTE str2hex(const char *str, BYTE *hex, size_t siz);
+extern int writer_FileInfoFile(void);
+extern int writer_OnlineProg(char *fileinfo);
+extern int writer_BootProg(char *fileinfo);
+extern BYTE *seq_search(BYTE *buff, size_t siz, char *target, size_t len);
+extern int writer_FpgaProg(void);
+extern int wt_ProgFileWrite(char *wt_dev, FILE *rd_fp);
+extern int wt_FpgaFileWrite(FILE *rd_fp);
+extern BYTE tftp(INNER_MSG *msg_p, uint16_t cnt);
+#endif // !DEBUG
 
 #endif /* _TFTP_TSK_H_ */
 

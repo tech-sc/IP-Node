1f33b1a8acd17478a9024de01ad93c392f2992e2
Sadohara <zz033130@oki.com>
2020-12-01 10:07:51 +0900

    [DEV-UT] 物理アドレス準備関数の用意

--- UnitTest/com_test.cpp
+++ UnitTest/com_test.cpp
@@ -5,12 +5,16 @@
 /*								Copyright 2020 Oki Electric Industry Co.,Ltd. */
 /******************************************************************************/
 //#include <sys/types.h>
-//#include <sys/stat.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
-#include <sys/memfd.h>		/* memfd_create() */
+#include <fcntl.h>
+#include <unistd.h>
+#include <inttypes.h>
 
 #include "gtest/gtest.h"
 
@@ -41,6 +45,8 @@ extern "C" {
 /******************************************************************************/
 /* テストクラス																  */
 /******************************************************************************/
+#define PFN_MASK_SIZE 8
+
 class ComMemLv01 : public ::testing::Test {
 protected:
 	ComMemLv01(){
@@ -54,6 +60,50 @@ protected:
 	// TestCase名ごとの後処理。テストケース実行後に実行される。
 	virtual void TearDown(){
 	}
+
+	unsigned long virt2phy(const void *virtaddr)
+	{
+	    unsigned long page, physaddr, virt_pfn;
+	    off_t offset;
+	    int page_size = sysconf(_SC_PAGESIZE);
+	    int fd, retval;
+
+	    fd = open("/proc/self/pagemap", O_RDONLY);
+	    if (fd < 0) {
+	        printf("error: open");
+	        return 0;
+	    }
+
+	    virt_pfn = (unsigned long)virtaddr / page_size;
+	    offset = sizeof(uint64_t) * virt_pfn;
+	    if (lseek(fd, offset, SEEK_SET) == (off_t) -1) {
+	        printf("error: lseek\n");
+	        close(fd);
+	        return 0;
+	    }
+
+	    retval = read(fd, &page, PFN_MASK_SIZE);
+	    close(fd);
+	    if (retval < 0) {
+	        printf("error: read\n");
+	        return 0;
+	    } else if (retval != PFN_MASK_SIZE) {
+	        printf("error: read2\n");
+	        return 0;
+	    }
+
+	    if ((page & 0x7fffffffffffffULL) == 0){
+	        printf("pfn == 0\n");
+	        printf("page = %016lX\n", page);
+	        return 0;
+	    }
+
+	    physaddr = ((page & 0x7fffffffffffffULL) * page_size)
+	        + ((unsigned long)virtaddr % page_size);
+
+	    return physaddr;
+	}
+
 };
 
 /******************************************************************************/
@@ -67,15 +117,16 @@ TEST_F(ComMemLv01, com_memInit)
 TEST_F(ComMemLv01, com_memRead)
 {
 	DWORD	data=0;
-	DWORD	*ptr=NULL;
+	DWORD	*virt_ptr=NULL, *phy_ptr=NULL;
 
-	int		fd = memfd_create("tmp_com_mem.txt", MFD_ALLOW_SEALING);
-	write(fd, "1234567890", 10);
-	ptr = mmap(0, MAP_SIZE, PROT_READ, MAP_SHARED, fd, 0 & ~MAP_MASK);
+	virt_ptr = malloc(256);
+	memcpy(virt_ptr, "1234567890", 10);
 
-	EXPECT_EQ(OK, com_memRead(ptr, DWORD_WIDTH, 1, &data));
+	phy_ptr = (DWORD*)virt2phy(virt_ptr);
+
+	EXPECT_EQ(OK, com_memRead(phy_ptr, DWORD_WIDTH, 1, &data));
 	printf("data=%lX\n", data);
-	munmap(ptr, MAP_SIZE);
-	close(fd);
+
+	free(virt_ptr);
 }
 
